24c614e740146df196aefd1917ed64a04d8df373
Sukbeom Kim <sukbeom.kim@gmail.com>
2024-12-10 00:37:44 +0900

    Tue Dec 10 12:37:44 AM KST 2024

--- deploy.sh
+++ deploy.sh
@@ -14,5 +14,11 @@ git commit -m "$(date)"
 git push origin main
 popd
 
+pushd themes/hugo-dead-simple
+git add .
+git commit -m "$(date)"
+git push private main
+popd
+
 git add . && git commit -m "$(date)"
 git push origin draft
