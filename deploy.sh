3b9b973e6a91e49ca7d2933470e82250775dbee3
Sukbeom Kim <sukbeom.kim@gmail.com>
2024-12-10 00:04:46 +0900

    Tue Dec 10 12:04:46 AM KST 2024

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,13 @@ pushd public
 git add .
 git commit -m "$(date)"
 git push origin master
-
 popd
+
+pushd content
+git add .
+git commit -m "$(date)"
+git push origin main
+popd
+
 git add . && git commit -m "$(date)"
 git push origin draft
